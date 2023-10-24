90bb72d35b74aaf6c55eab6a84b0378d9eaf24cd
Naulian <cinkhangin@gmail.com>
2023-10-24 19:49:47 +0630

    update libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,36 +44,34 @@ android {
 }
 
 dependencies {
-
-    //android(ktx
+    //androidx
     implementation("androidx.core:core-ktx:1.12.0")
-
-    //app compat
     implementation("androidx.appcompat:appcompat:1.6.1")
-
-    //preference
-    implementation("androidx.preference:preference-ktx:1.2.1")
-
-    //material
-    implementation("com.google.android.material:material:1.9.0")
-
-    //constraint
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-
-    implementation(platform("com.google.firebase:firebase-bom:32.0.0"))
-    implementation("com.google.firebase:firebase-auth-ktx")
-    implementation("com.google.firebase:firebase-firestore-ktx")
-
-    val navVersion = "2.7.3"
+    implementation("androidx.preference:preference-ktx:1.2.1")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    //navigation
+    val navVersion = "2.7.4"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
-
     //lifecycle
     val lifecycleVersion = "2.6.2"
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    //room
+    val roomVersion = "2.6.0"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+
+    //google
+    implementation("com.google.android.material:material:1.10.0")
+    //firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.0.0"))
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
 
     //dagger hilt
     val hiltVersion = "2.48"
@@ -83,23 +81,12 @@ dependencies {
     //glide
     implementation("com.github.bumptech.glide:glide:4.15.1")
 
-    //datastore
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-
-    //splashscreen
-    implementation("androidx.core:core-splashscreen:1.0.1")
-
-    //anhance
-    implementation("com.github.cinkhangin:anhance:1.3.3")
+    //cinkhangin
+    implementation("com.github.cinkhangin:anhance:1.3.4")
     implementation("com.github.cinkhangin:firex:1.2.0")
-    implementation("com.github.cinkhangin:glow:0.2.0")
+    implementation("com.github.cinkhangin:glow:1.2.1")
 
+    //jetbrains
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
-
-    //room
-    implementation("androidx.room:room-runtime:2.5.2")
-    implementation("androidx.room:room-ktx:2.5.2")
-    ksp("androidx.room:room-compiler:2.5.2")
-
 }
 
